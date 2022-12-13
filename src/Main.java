6e2ddaec931c7bb9ed6287d66d232c07c6390328
Peter Verčimák <99815989+PetoVerci@users.noreply.github.com>
2022-12-13 13:10:39 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/form_versions", "root", "root");
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/moja", "root", "root");
             System.out.println("Success");
 
             if (connection != null) {
                 DbContext.setConnection(connection);
 
-                CatiaSheet cs = PDFParser.parseFile("src/cat2.pdf");
-
+                CatiaSheet cs = PDFParser.parseFile("src/cat3.pdf");
 
             } else {
                 System.out.println("Failed to make connection!");
@@ -22,6 +28,16 @@ public class Main {
             throw new RuntimeException(e);
         } catch (Exception e) {
             e.printStackTrace();
+
         }
+        launch(args);
     }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("versionOne.fxml"));
+        stage.setTitle("BogeParser");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 }
